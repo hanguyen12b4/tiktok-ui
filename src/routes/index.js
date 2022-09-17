a408b8dabc52e0909a2b4dd83c7e172610b738f3
hanguyen <hanguyen12b4@gmail.com>
2022-09-17 11:18:05 +0700

    Cấu hình Router(tt)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
 
 //publicRoutes
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 //privateRoutes
 const privateRoutes = [];
